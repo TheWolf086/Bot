32173978fb3ab68334bf22d91b098770896cfae3
TheWolf086 <46865082+TheWolf086@users.noreply.github.com>
2019-01-20 14:16:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const kaamelott = require('./commands/kaamelott')
 const kaamelotthandler = require('./commands/kaamelotthandler')
 const son = require('./commands/sounds.json')
 const leave = require('./commands/sounds.json')
+const TOKEN = process.env.token
 
 bot.on('message', function (message) {
   let comandUsed = kaamelott.parse(message) || kaamelotthandler.parse(message)
 })
 
-bot.login('NTMyNjIwMzczMjYzNTE1NjQ4.DxfLTQ.OA3AMbX1klCfsJ9ahjQasW_38r0')
+bot.login(TOKEN)
