d47d19abb09a446036734141ebf7290973a78c77
TheWolf086 <46865082+TheWolf086@users.noreply.github.com>
2019-01-20 14:22:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+const TOKEN = process.env.token;
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const kaamelott = require('./commands/kaamelott')
 const kaamelotthandler = require('./commands/kaamelotthandler')
 const son = require('./commands/sounds.json')
 const leave = require('./commands/sounds.json')
-const TOKEN = process.env.token;
 
 bot.on('message', function (message) {
   let comandUsed = kaamelott.parse(message) || kaamelotthandler.parse(message)
 })
-
 
 bot.login(TOKEN)
