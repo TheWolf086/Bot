049092ba0858f50068a06e2c23a5a61ddbc75bcb
TheWolf086 <46865082+TheWolf086@users.noreply.github.com>
2019-03-13 15:06:44 +0100

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -9,6 +9,6 @@ module.exports = class leave extends Command {
       auteur = message.author
       voice_channel = auteur.voice_channel
         client.join_voice_channel(voice_channel).disconnect()
-        message.channel.send("Déco")
+        {connection.disconnect()}
     }
 }
